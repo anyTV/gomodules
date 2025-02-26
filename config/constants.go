49db52de863434ee7898386760f76eea480a14df
Sherwin Ferrer <wildtron@users.noreply.github.com>
2025-02-26 18:40:35 +0800

    feat: add config/constants.go

---
+++ config/constants.go
@@ -0,0 +1,11 @@
+package config
+
+const (
+	MinUsernameLen = 8
+	UsernameSuffix = "abc123"
+	EmptyString    = ""
+	BitSize        = 4096
+
+	PromptNone          = "none"
+	ResponseTypeIdToken = "id_token"
+)
